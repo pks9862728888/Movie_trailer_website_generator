a792af6cef614a8e00721e1e35ca1f3235720d52
pks9862728888 <pks9862728888@gmail.com>
2019-01-21 20:09:24 +0530

    followed PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -30,25 +30,28 @@ def get_trailer_data(data, object_name, flag=0):
     poster_image_url = data.loc[object_name, 'poster_image_url']
     release_date = data.loc[object_name, 'release_date']
 
-    # For maintaining fixed height of grids restricting story_line to 136 characters.
+    # Restricting story_line to 136 characters for fixed grid size.
     if len(story_line) > 136:
         story_line = story_line[0:136] + "..."
 
     # Getting season information if Tv series
     if flag:
         season = data.loc[object_name, 'season']
-        return [name, story_line, yt_trailer_url, poster_image_url, release_date, season]
+        return [name, story_line, yt_trailer_url,
+                poster_image_url, release_date, season]
     else:
-        return [name, story_line, yt_trailer_url, poster_image_url, release_date]
+        return [name, story_line, yt_trailer_url,
+                poster_image_url, release_date]
 
 
 def main():
     """
-    Creates Movie objects and initializes these objects with
-    title, story_line, you_tube_trailer_url, poster_image_url, and release_date.
+    Creates Movie objects and initializes these objects with title,
+    story_line, you_tube_trailer_url, poster_image_url, & release_date.
 
     Also creates Tv series objects and initializes these objects with
-    title, story_line, you_tube_trailer_url, poster_image_url, release_date, season.
+    title, story_line, you_tube_trailer_url, poster_image_url,
+    release_date, and season.
     """
     # Reading movies and TvSeries datafiles
     data_movie = pd.read_csv("datafile_movie.csv", index_col='title')
@@ -65,55 +68,68 @@ def main():
 
     movie_data = get_trailer_data(data_movie, "Automata")
     automata = media.Movie(movie_data[0], movie_data[1],
-                           movie_data[2], movie_data[3], movie_data[4])
+                           movie_data[2], movie_data[3],
+                           movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Extinction")
-    extinction = media.Movie(movie_data[0], movie_data[1], movie_data[2],
-                             movie_data[3], movie_data[4])
+    extinction = media.Movie(movie_data[0], movie_data[1],
+                             movie_data[2], movie_data[3],
+                             movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Stealth")
-    stealth = media.Movie(movie_data[0], movie_data[1], movie_data[2],
-                          movie_data[3], movie_data[4])
+    stealth = media.Movie(movie_data[0], movie_data[1],
+                          movie_data[2], movie_data[3],
+                          movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Maze Runner: The Death Cure")
     maze_runner_the_death_cure = media.Movie(movie_data[0], movie_data[1],
-                                             movie_data[2], movie_data[3], movie_data[4])
+                                             movie_data[2], movie_data[3],
+                                             movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Maze Runner: The Scorch Trials")
     maze_runner_the_scorch_trials = media.Movie(movie_data[0], movie_data[1],
-                                                movie_data[2], movie_data[3], movie_data[4])
+                                                movie_data[2], movie_data[3],
+                                                movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "The Maze Runner")
     the_maze_runner = media.Movie(movie_data[0], movie_data[1],
-                                  movie_data[2], movie_data[3], movie_data[4])
+                                  movie_data[2], movie_data[3],
+                                  movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Interstellar")
     interstellar = media.Movie(movie_data[0], movie_data[1],
-                               movie_data[2], movie_data[3], movie_data[4])
+                               movie_data[2], movie_data[3],
+                               movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Pacific Rim")
     pacific_rim = media.Movie(movie_data[0], movie_data[1],
-                              movie_data[2], movie_data[3], movie_data[4])
+                              movie_data[2], movie_data[3],
+                              movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Pacific Rim: Uprising")
     pacific_rim_uprising = media.Movie(movie_data[0], movie_data[1],
-                                       movie_data[2], movie_data[3], movie_data[4])
+                                       movie_data[2], movie_data[3],
+                                       movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Lucy")
     lucy = media.Movie(movie_data[0], movie_data[1],
-                       movie_data[2], movie_data[3], movie_data[4])
+                       movie_data[2], movie_data[3],
+                       movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Prometheus")
     prometheus = media.Movie(movie_data[0], movie_data[1],
-                             movie_data[2], movie_data[3], movie_data[4])
+                             movie_data[2], movie_data[3],
+                             movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Edge of Tomorrow")
     edge_of_tomorrow = media.Movie(movie_data[0], movie_data[1],
-                                   movie_data[2], movie_data[3], movie_data[4])
+                                   movie_data[2], movie_data[3],
+                                   movie_data[4])
 
     movie_data = get_trailer_data(data_movie, "Inception")
     inception = media.Movie(movie_data[0], movie_data[1],
-                            movie_data[2], movie_data[3], movie_data[4])
+                            movie_data[2], movie_data[3],
+                            movie_data[4])
 
     # Creating TvSeries objects
     series_data = get_trailer_data(data_tv_series, "Westworld: S1", 1)
@@ -125,29 +141,35 @@ def main():
                                 series_data[3], series_data[4], series_data[5])
 
     series_data = get_trailer_data(data_tv_series, "Intelligence: S1", 1)
-    intelligence_s1 = media.TvShow(series_data[0], series_data[1], series_data[2],
-                                   series_data[3], series_data[4], series_data[5])
+    intelligence_s1 = media.TvShow(series_data[0], series_data[1],
+                                   series_data[2], series_data[3],
+                                   series_data[4], series_data[5])
 
     series_data = get_trailer_data(data_tv_series, "Intelligence: S2", 1)
-    intelligence_s2 = media.TvShow(series_data[0], series_data[1], series_data[2],
-                                   series_data[3], series_data[4], series_data[5])
+    intelligence_s2 = media.TvShow(series_data[0], series_data[1],
+                                   series_data[2], series_data[3],
+                                   series_data[4], series_data[5])
 
     series_data = get_trailer_data(data_tv_series, "Minority Report", 1)
-    minority_report = media.TvShow(series_data[0], series_data[1], series_data[2],
-                                   series_data[3], series_data[4], series_data[5])
+    minority_report = media.TvShow(series_data[0], series_data[1],
+                                   series_data[2], series_data[3],
+                                   series_data[4], series_data[5])
 
     series_data = get_trailer_data(data_tv_series, "Almost Human", 1)
-    almost_human = media.TvShow(series_data[0], series_data[1], series_data[2],
-                                series_data[3], series_data[4], series_data[5])
+    almost_human = media.TvShow(series_data[0], series_data[1],
+                                series_data[2], series_data[3],
+                                series_data[4], series_data[5])
 
     # Creating movie trailer website
     fresh_tomatoes.open_movies_page([axl, tau, automata, extinction, stealth,
-                                     maze_runner_the_death_cure, maze_runner_the_scorch_trials,
-                                     the_maze_runner, interstellar, pacific_rim,
-                                     pacific_rim_uprising, lucy, prometheus,
-                                     edge_of_tomorrow, inception, westworld_s1, westworld_s2,
-                                     intelligence_s1, intelligence_s2, minority_report,
-                                     almost_human])
+                                     maze_runner_the_death_cure,
+                                     maze_runner_the_scorch_trials,
+                                     the_maze_runner, interstellar,
+                                     pacific_rim, pacific_rim_uprising,
+                                     lucy, prometheus, edge_of_tomorrow,
+                                     inception, westworld_s1, westworld_s2,
+                                     intelligence_s1, intelligence_s2,
+                                     minority_report, almost_human])
 
 
 if __name__ == '__main__':
